f2bd22e9786927eb8c51930253499c1cfe241caa
Justin Reina <justinmreina@gmail.com>
2025-03-02 18:55:23 -0800

    (C)    compile & deploy

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Documents/MyWorkspaces/Espressif/Espressif_RTOS_Demo/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "D:/Documents/MyWorkspaces/Espressif/.git")
+set(GIT_DIR "D:/Documents/MyWorkspaces/Espressif/Espressif_RTOS_Demo/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
